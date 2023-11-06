1986cde4eb6be7a0255186123edbd5c8d0aeecdf
Riccardo Benevelli <rickybenevelli@gmail.com>
2023-11-06 11:54:48 +0100

    tabella utenti nella dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,13 +6,18 @@ import Image from "next/image";
 import { compareDesc, format, parseISO } from "date-fns";
 
 import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+
+import { columns } from "@/app/dashboard/columns";
+import { DataTable } from "@/app/dashboard/data-table";
 
 export default async function Home() {
   const session = await getAuthSession();
+  const data = await db.user.findMany();
 
   return (
-    <div>
-      Ciao
+    <div className="w-full m-auto">
+      <DataTable columns={columns} data={data as any} />
     </div>
   );
 }
