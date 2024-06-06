50c23c3ab7a52a0d813997ba7703ff4479c82964
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-06-06 15:58:15 +0200

    feat: add new episode pt3

--- components/SpotifyEpisodes.tsx
+++ components/SpotifyEpisodes.tsx
@@ -21,6 +21,14 @@ const SpotifyEpisodes = () => {
         className="w-full md:w-1/3 aspect-[9/16]"
       ></iframe>
 
+      <iframe
+        style={ { borderRadius: '12px' } }
+        src="https://open.spotify.com/embed/episode/03RlajYvtAoBQY71RWXlNp/video?utm_source=generator"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        className="w-full md:w-1/3 aspect-[9/16]"
+      ></iframe>
+
     </div>
   );
 };
