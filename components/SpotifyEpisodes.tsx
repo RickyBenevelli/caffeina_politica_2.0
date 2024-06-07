5ffb9ed657a1203709aac6779aa44e184fff40d2
Riccardo Benevelli <rickybenevelli@gmail.com>
2024-06-07 09:21:09 +0200

    feat: add new episode pt4 and pt5

--- components/SpotifyEpisodes.tsx
+++ components/SpotifyEpisodes.tsx
@@ -3,30 +3,51 @@ import React from 'react';
 
 const SpotifyEpisodes = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-4 pt-10">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-10">
 
+      {/* Fabrizio Aguzzoli - Coalizione civica */}
       <iframe
         style={ { borderRadius: '12px' } }
         src="https://open.spotify.com/embed/episode/5tWHqfaZw4yEErdswEMsH2/video?utm_source=generator"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
-        className="w-full md:w-1/3 aspect-[9/16]"
+        className="w-full aspect-[9/16]"
       ></iframe>
 
+      {/* Marco Massari - Coalizione Centro Sinistra */}
       <iframe
         style={ { borderRadius: '12px' } }
         src="https://open.spotify.com/embed/episode/3f8JDNnSgMmzEGCQrkjy5g/video?utm_source=generator"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
-        className="w-full md:w-1/3 aspect-[9/16]"
+        className="w-full aspect-[9/16]"
       ></iframe>
 
+      {/* Giuliana Reggio - Alleanza Civica */}
       <iframe
         style={ { borderRadius: '12px' } }
         src="https://open.spotify.com/embed/episode/03RlajYvtAoBQY71RWXlNp/video?utm_source=generator"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
-        className="w-full md:w-1/3 aspect-[9/16]"
+        className="w-full aspect-[9/16]"
+      ></iframe>
+
+      {/* Gianni Tasselli - REagire */}
+      <iframe
+        style={ { borderRadius: '12px' } }
+        src="https://open.spotify.com/embed/episode/44t14bjd4KIsj8ofNjTAO4/video?utm_source=generator"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        className="w-full aspect-[9/16]"
+      ></iframe>
+
+      {/* Paolo Soragni - Movimento per Reggio Emilia */}
+      <iframe
+        style={ { borderRadius: '12px' } }
+        src="https://open.spotify.com/embed/episode/25fecgqenhfpvZSworvZTA/video?utm_source=generator"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        className="w-full aspect-[9/16]"
       ></iframe>
 
     </div>
